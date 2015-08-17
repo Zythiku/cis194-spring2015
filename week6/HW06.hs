75ae76c631cbbf95b13ef8dae12775bba0f097b2
Zythiku <zythiku@gmail.com>
2015-08-16 22:35:01 -0700

    Finished hw 6

--- week6/HW06.hs
+++ week6/HW06.hs
@@ -2,20 +2,23 @@
 module HW06 where
 
 import Data.List
-import Data.Functor
+import Data.Functor()
 
 -- Exercise 1 -----------------------------------------
 
 fib :: Integer -> Integer
-fib = undefined
+fib num
+    | num <= 0 = 0
+    | num == 1 = 1
+    | otherwise = fib (num - 1) + fib (num - 2)
 
 fibs1 :: [Integer]
-fibs1 = undefined
+fibs1 = map fib [1..]
 
 -- Exercise 2 -----------------------------------------
 
 fibs2 :: [Integer]
-fibs2 = undefined
+fibs2 = 1 : 1 : zipWith (+) fibs2 (tail fibs2)
 
 -- Exercise 3 -----------------------------------------
 
@@ -27,56 +30,78 @@ instance Show a => Show (Stream a) where
              ++ ",..."
 
 streamToList :: Stream a -> [a]
-streamToList = undefined
+streamToList (Cons a stream) = a : streamToList stream
 
 -- Exercise 4 -----------------------------------------
 
 instance Functor Stream where
-    fmap = undefined
+    fmap f (Cons a stream) = Cons (f a) (fmap f stream)
 
 -- Exercise 5 -----------------------------------------
 
 sRepeat :: a -> Stream a
-sRepeat = undefined
+sRepeat a = Cons a (sRepeat a)
 
 sIterate :: (a -> a) -> a -> Stream a
-sIterate = undefined
+sIterate f a = Cons (f a) (sIterate f (f a))
 
 sInterleave :: Stream a -> Stream a -> Stream a
-sInterleave (Cons _ _) _ = undefined
+sInterleave (Cons a stream) = Cons a . flip sInterleave stream
 
 sTake :: Int -> Stream a -> [a]
-sTake = undefined
+sTake n (Cons a stream)
+    | n <= 0 = []
+    | otherwise = a : sTake (n-1) stream
 
 -- Exercise 6 -----------------------------------------
 
 nats :: Stream Integer
-nats = undefined
+nats = Cons 0 $ fmap (+1) nats
 
 ruler :: Stream Integer
-ruler = undefined
+ruler = ruler' 0
+    where ruler' num = sInterleave (sRepeat num) $ ruler' (num + 1)
 
 -- Exercise 7 -----------------------------------------
 
 -- | Implementation of C rand
 rand :: Int -> Stream Int
-rand = undefined
+rand num
+    | num >= 1 = Cons randomNum (rand (num + 1))
+    | otherwise = rand 1
+    where randomNum = (1103515245 * (num - 1) + 12345) `mod` 2147483648
+
 
 -- Exercise 8 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: 234 MB -}
 minMaxSlow :: [Int] -> Maybe (Int, Int)
 minMaxSlow [] = Nothing   -- no min or max if there are no elements
 minMaxSlow xs = Just (minimum xs, maximum xs)
 
 -- Exercise 9 -----------------------------------------
 
-{- Total Memory in use: ??? MB -}
+{- Total Memory in use: 1 MB -}
 minMax :: [Int] -> Maybe (Int, Int)
-minMax = undefined
+minMax [] = Nothing
+minMax (x:xs) = Just (minMax' (x, x) xs)
+    where minMax' (low, high) (y:ys) = low `seq` high `seq` minMax' (min y (low), max y (high)) ys
+          minMax' pair _ = pair
+
+{- Total Memory in use: 212 MB -}
+minMaxFold :: [Int] -> Maybe (Int, Int)
+minMaxFold [] = Nothing
+minMaxFold (x:xs) = Just (foldl' (\(low, high) num -> (min low num, max high num)) base xs)
+    where base = (x, x)
+
+{- Total Memory in use: 274 MB -}
+minMaxFoldr :: [Int] -> Maybe (Int, Int)
+minMaxFoldr [] = Nothing
+minMaxFoldr(x:xs) = Just (foldr (\num (low, high) -> (min low num, max high num)) base xs)
+    where base = (x, x)
 
 main :: IO ()
-main = print $ minMaxSlow $ sTake 1000000 $ rand 7666532
+main = print $ minMaxFold $ sTake 1000000 $ rand 7666532
 
 -- Exercise 10 ----------------------------------------
 
